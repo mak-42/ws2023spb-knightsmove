7eb93606eea728100c0b83fba1a8cca7db8bd853
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-03 22:24:29 +0300

    feat: create IllegalMoveException for validating moves

--- src/main/java/croc/education/ws2023spb/knightsmove/IllegalMoveException.java
+++ src/main/java/croc/education/ws2023spb/knightsmove/IllegalMoveException.java
@@ -7,5 +7,20 @@ package croc.education.ws2023spb.knightsmove;
  * @author Dmitry Malenok
  */
 public class IllegalMoveException extends Exception {
-    // TODO: наполнить класс.
+    private final ChessPosition from;
+    private final ChessPosition to;
+
+    public IllegalMoveException(ChessPosition from, ChessPosition to) {
+        super("конь так не ходит: " + from + " -> " + to);
+        this.from = from;
+        this.to = to;
+    }
+
+    public ChessPosition getFrom() {
+        return from;
+    }
+
+    public ChessPosition getTo() {
+        return to;
+    }
 }
