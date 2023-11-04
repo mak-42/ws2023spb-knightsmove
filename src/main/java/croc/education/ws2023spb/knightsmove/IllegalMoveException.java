3848353fbd3ad36ca9b5be4671d3764ff1cbd2b2
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-04 18:49:26 +0300

    refactor: update IllegalMoveException

--- src/main/java/croc/education/ws2023spb/knightsmove/IllegalMoveException.java
+++ src/main/java/croc/education/ws2023spb/knightsmove/IllegalMoveException.java
@@ -7,20 +7,7 @@ package croc.education.ws2023spb.knightsmove;
  * @author Dmitry Malenok
  */
 public class IllegalMoveException extends Exception {
-    private final ChessPosition from;
-    private final ChessPosition to;
-
     public IllegalMoveException(ChessPosition from, ChessPosition to) {
         super("конь так не ходит: " + from + " -> " + to);
-        this.from = from;
-        this.to = to;
-    }
-
-    public ChessPosition getFrom() {
-        return from;
-    }
-
-    public ChessPosition getTo() {
-        return to;
     }
 }
