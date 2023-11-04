355c2ffce2c3b999637066f28c475db11dd7b9d7
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-04 18:48:00 +0300

    refactor: update IllegalPositionException from Exception to RuntimeException

--- src/main/java/croc/education/ws2023spb/knightsmove/IllegalPositionException.java
+++ src/main/java/croc/education/ws2023spb/knightsmove/IllegalPositionException.java
@@ -1,6 +1,6 @@
 package croc.education.ws2023spb.knightsmove;
 
-public class IllegalPositionException extends Exception {
+public class IllegalPositionException extends RuntimeException {
     public IllegalPositionException(String message) {
         super(message);
     }
