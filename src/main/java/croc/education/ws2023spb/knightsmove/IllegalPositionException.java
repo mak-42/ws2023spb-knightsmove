018d0ddf39fe42bf97778fe5d136d4f93165edec
Valery Volodin <valery_volo@mail.ru>
2023-11-07 00:02:45 +0500

    Добавлены информативные исключения.

--- src/main/java/croc/education/ws2023spb/knightsmove/IllegalPositionException.java
+++ src/main/java/croc/education/ws2023spb/knightsmove/IllegalPositionException.java
@@ -1,7 +1,22 @@
 package croc.education.ws2023spb.knightsmove;
 
-public class IllegalPositionException extends RuntimeException{
-    public IllegalPositionException(String errorMessage){
-        super(errorMessage);
+public class IllegalPositionException extends RuntimeException {
+    public IllegalPositionException(String position) {
+        super("Неверно заданная длина шахматной аннотации: " + position + " - введите позицию двумя литералами");
+
+    }
+
+    public IllegalPositionException(int x, int y) {
+        super("Неверно заданные координаты: " + x + ", " + y + " - введите цифры от 0 до 7.");
+
+    }
+
+    public IllegalPositionException(String position, char number) {
+        super("Неверно определена: " + position  +  " - введите вместо второго литерала \"" + number + "\" от 1 до 8.");
+    }
+
+
+    public IllegalPositionException(char letter, String position) {
+        super("Неверно определена: " + position  + " - введите вместо второго литерала \"" + letter + "\" латинскую букву от a до h.");
     }
 }
