25d71da7545dc8774c5c2e0511f00a00ebf2e020
I'm DaK0Ta <arsbroart@mail.ru>
2023-11-12 15:24:50 +0300

    feat(ChessboardPosition): creat a single positionXToLetter constant and remove unnecessary checking of position values

--- src/main/java/croc/education/ws2023spb/knightsmove/ChessboardPosition.java
+++ src/main/java/croc/education/ws2023spb/knightsmove/ChessboardPosition.java
@@ -7,29 +7,19 @@ import java.util.Map;
 public class ChessboardPosition implements ChessPosition {
     private final int columnX;
     private final int rowY;
-    static final int MIN_POSITION = 0;
-    static final int MAX_POSITION = 7;
-    private static final Map<Integer, String> mutablePositionXToLetter = new HashMap<>();
-    static {
-        mutablePositionXToLetter.put(0, "a");
-        mutablePositionXToLetter.put(1, "b");
-        mutablePositionXToLetter.put(2, "c");
-        mutablePositionXToLetter.put(3, "d");
-        mutablePositionXToLetter.put(4, "e");
-        mutablePositionXToLetter.put(5, "f");
-        mutablePositionXToLetter.put(6, "g");
-        mutablePositionXToLetter.put(7, "h");
-    }
-    static Map<Integer, String> positionXToLetter = Collections.unmodifiableMap(mutablePositionXToLetter);
+    private static final Map<Integer, String> positionXToLetter = Map.of(
+        0, "a",
+        1, "b",
+        2, "c",
+        3, "d",
+        4, "e",
+        5, "f",
+        6, "g",
+        7, "h"
+    );
 
+    
     public ChessboardPosition(int columnX, int rowY) {
-        if (columnX < MIN_POSITION || columnX > MAX_POSITION) {
-            throw new IllegalPositionException("Позиция X должна находиться в диапазоне от " + MIN_POSITION + " до " + MAX_POSITION);
-        }
-        if (rowY < MIN_POSITION || rowY > MAX_POSITION) {
-            throw new IllegalPositionException("Позиция Y должна находиться в диапазоне от " + MIN_POSITION + " до " + MAX_POSITION);
-        }
-
         this.columnX = columnX;
         this.rowY = rowY;
     }
